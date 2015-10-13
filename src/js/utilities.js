ca3a89b441cbbc89520bf1004fd67915df659361
Jack Murphy <jmurphy.jr@gmail.com>
2015-10-13 06:41:53 -0400

    feat: Added a function to calculate the frames per second for debugging purposes.

--- src/js/utilities.js
+++ src/js/utilities.js
@@ -248,3 +248,25 @@ var calculateLoopPoints = function() {
 
     return [points, ang];
 };
+
+/**
+ * Grabbed snippet of code from: http://www.html5gamedevs.com/topic/1828-how-to-calculate-fps-in-plain-javascript/
+ * @type {{startTime: number, frameNumber: number, getFPS: Function}}
+ */
+var fps = {
+    startTime : 0,
+    frameNumber : 0,
+    getFPS : function() {
+        this.frameNumber++;
+        var d = new Date().getTime();
+        var currentTime = (d - this.startTime) / 1000;
+        var result = Math.floor((this.frameNumber / currentTime));
+
+        if (currentTime > 1) {
+            this.startTime = new Date().getTime();
+            this.frameNumber = 0;
+        }
+        return result;
+
+    }
+};
